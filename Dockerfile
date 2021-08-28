86e58970b86e7dcac091858f0f1b850a23cf22a3
devMatheus <pereira.matheusalves@gmail.com>
2021-08-27 22:37:37 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+From node:14.15.4-alpine3.12
+
+RUN apk add --no-cache bash
+
+USER node
+
+WORKDIR /home/node/app
---
+++ src/shared/http/server.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res
+      .status(error.status_code)
+      .json({ status: 'error', message: error.message });
+  }
+
+  return res.status(500).json({ status: 'error', message: 'unknown error' });
+});
+
+app.listen(3333, () => {
+  console.log('server started on port 3333!');
+});
